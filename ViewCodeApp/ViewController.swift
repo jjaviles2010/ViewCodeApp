9aa0a5493efe1b2dbd67d255685cc01c42b6f9f4
Jose Javier Aviles Gomez <jose.aviles@nibspe.com.br>
2019-11-07 19:44:10 -0300

    Initial Commit

---
+++ ViewCodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewCodeApp
+//
+//  Created by Jose Javier on 07/11/19.
+//  Copyright © 2019 JLApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
